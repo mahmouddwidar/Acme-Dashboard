20b2d4dda705322b8e9561d129ff2edbbbb36005
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2025-04-09 10:09:50 +0200

    feat: added invocies page. - added the structure of invocies page. - added search functionality with user search query params and page number. - added pagination.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   ppr: 'incremental' # for Partial Prerendering and 'incremental' value allows you to adopt PPR for specific routes.
+  // }
 };
 
 export default nextConfig;
