0f96b39684f30802e23ed58e30ded7c8af61ebec
Mahmoud Dwidar <mahmouddwidar55@gmail.com>
2025-04-10 10:06:56 +0200

    feat: implement customers table with search and pagination.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,27 @@
+import { TableRowSkeleton } from "@/app/ui/skeletons";
 import { Metadata } from "next";
-import React from "react";
+import { Suspense } from "react";
+import Table from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
-	title: 'Customers'
-}
+	title: "Customers",
+};
 
-export default function page() {
-	return <p>Customers Page</p>;
+export default async function page(props: {
+	searchParams?: Promise<{
+		query?: string;
+		page?: string;
+	}>;
+}) {
+	const searchParams = await props.searchParams;
+	const query = searchParams?.query || "";
+	const currentPage = Number(searchParams?.page) || 1;
+	const customers = await fetchFilteredCustomers(query);
+
+	return (
+		<Suspense key={query + currentPage} fallback={<TableRowSkeleton />}>
+			<Table customers={customers} />
+		</Suspense>
+	);
 }
